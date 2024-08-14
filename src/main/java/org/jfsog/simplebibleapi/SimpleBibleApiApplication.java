d0e8993fbc332a1bcf8615ce0323232728664589
jfsog <Felipe.sg@protonmail.com>
2024-08-14 12:03:00 -0300

    Commit inicial

---
+++ src/main/java/org/jfsog/simplebibleapi/SimpleBibleApiApplication.java
@@ -0,0 +1,11 @@
+package org.jfsog.simplebibleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBibleApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBibleApiApplication.class, args);
+    }
+}
---
+++ src/test/java/org/jfsog/simplebibleapi/SimpleBibleApiApplicationTests.java
@@ -0,0 +1,11 @@
+package org.jfsog.simplebibleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBibleApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
